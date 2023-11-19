2e78f138581610704a65091053d43d175413d0a9
Taiobaaa <109032098+Taiobaaa@users.noreply.github.com>
2023-11-19 16:28:05 -0300

    Create 4c.cpp

---
+++ 4c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+   //. Fac¸a um programa que receba dois numeros e mostre qual deles ´ e o maior
+   int X,Y;
+   cin>> X;
+   cin>> Y;
+   if (X>Y){
+   cout<<X;
+   }else{
+       if(X<Y){
+           cout<<Y;
+       }else{
+          cout<<"igual";
+              
+           
+       }
+   }
