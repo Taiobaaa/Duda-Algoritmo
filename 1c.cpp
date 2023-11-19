1a56f019666645ecf4a708d47f078fa78f35817d
Taiobaaa <109032098+Taiobaaa@users.noreply.github.com>
2023-11-19 16:25:07 -0300

    Create 1c.cpp

---
+++ 1c.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+   int X;
+   cin >> X;
+   if (X >= 1){
+   cout << "positivo";
+   }else{
+       if(X < 0){
+           cout << "negativo";
+       }else{
+          cout << "igual";
+              
+           
+       }
+   }
+    
+    
+    
+    
+    return 0;
+}
